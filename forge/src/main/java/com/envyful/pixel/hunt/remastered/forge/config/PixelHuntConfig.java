3d8a79bd3d454317cfab5da614bb4142d87c76a7
danorris709 <dnorris443@gmail.com>
2021-09-01 15:54:46 +0100

    feat(forge): update PixelHuntConfig to new config

--- forge/src/main/java/com/envyful/pixel/hunt/remastered/forge/config/PixelHuntConfig.java
+++ forge/src/main/java/com/envyful/pixel/hunt/remastered/forge/config/PixelHuntConfig.java
@@ -2,6 +2,7 @@ package com.envyful.pixel.hunt.remastered.forge.config;
 
 import com.envyful.api.config.data.ConfigPath;
 import com.envyful.api.config.type.ConfigInterface;
+import com.envyful.api.config.yaml.AbstractYamlConfig;
 import com.google.common.collect.Lists;
 import org.spongepowered.configurate.objectmapping.ConfigSerializable;
 
@@ -9,12 +10,16 @@ import java.util.List;
 
 @ConfigPath("config/PixelHuntRemastered/config.yml")
 @ConfigSerializable
-public class PixelHuntConfig {
+public class PixelHuntConfig extends AbstractYamlConfig {
 
     private ConfigInterface configInterface = new ConfigInterface();
 
     private List<String> spawnBroadcast = Lists.newArrayList();
     private List<String> timeoutBroadcast = Lists.newArrayList();
+
+    public PixelHuntConfig() {
+        super();
+    }
 
     public List<String> getSpawnBroadcast() {
         return this.spawnBroadcast;
