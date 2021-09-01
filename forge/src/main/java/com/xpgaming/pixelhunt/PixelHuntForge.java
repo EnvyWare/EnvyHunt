d74691b7c9767e52349daa397d206e8a482d0aef
danorris709 <dnorris443@gmail.com>
2021-09-01 15:23:08 +0100

    ref(forge): gut main class

--- forge/src/main/java/com/xpgaming/pixelhunt/PixelHuntForge.java
+++ forge/src/main/java/com/xpgaming/pixelhunt/PixelHuntForge.java
@@ -1,7 +1,6 @@
 package com.xpgaming.pixelhunt;
 
-import com.envyful.acaf.api.CommandFactory;
-import com.envyful.acaf.impl.ForgeCommandFactory;
+import com.envyful.api.forge.command.ForgeCommandFactory;
 import com.pixelmonmod.pixelmon.Pixelmon;
 import com.xpgaming.pixelhunt.commands.PixelHuntCommand;
 import com.xpgaming.pixelhunt.config.PixelHuntConfig;
@@ -11,8 +10,6 @@ import com.xpgaming.pixelhunt.listener.PokemonCaptureListener;
 import com.xpgaming.pixelhunt.listener.PokemonSpawnListener;
 import com.xpgaming.pixelhunt.task.ParticleDisplayTask;
 import net.minecraft.server.MinecraftServer;
-import net.minecraft.util.text.ITextComponent;
-import net.minecraft.util.text.TextComponentString;
 import net.minecraftforge.common.MinecraftForge;
 import net.minecraftforge.fml.common.Mod;
 import net.minecraftforge.fml.common.event.FMLPreInitializationEvent;
@@ -24,25 +21,20 @@ import java.io.InputStream;
 import java.nio.file.Files;
 
 @Mod(
-        modid = PixelHuntForge.MOD_ID,
-        name = PixelHuntForge.MOD_NAME,
+        modid = "pixelhuntremastered",
+        name = "PixelHuntRemastered Forge",
         version = PixelHuntForge.MOD_VERSION,
         acceptableRemoteVersions = "*",
-        dependencies = "required-after:pixelmon;required-after:gooeylibs"
+        dependencies = "required-after:pixelmon;"
 )
 public class PixelHuntForge {
 
-    public static final String MOD_ID = "pixelhunt";
-    public static final String MOD_NAME = "PixelHunt Forge";
-    public static final String MOD_VERSION = "1.12.2-forge-1.0.1";
-
-    private static final ITextComponent NO_SUCH_HUNT = new TextComponentString("Cannot find a PokeHunt by that id");
+    public static final String MOD_VERSION = "0.1.0";
 
     private static PixelHuntForge instance;
-    private static MinecraftServer server;
 
     private final ParticleDisplayTask displayTask = new ParticleDisplayTask();
-    private final CommandFactory commandFactory = new ForgeCommandFactory();
+    private final ForgeCommandFactory commandFactory = new ForgeCommandFactory();
 
     private PixelHuntConfig config;
 
@@ -53,48 +45,7 @@ public class PixelHuntForge {
 
     @Mod.EventHandler
     public void onServerStarting(FMLServerStartingEvent event) {
-        server = event.getServer();
-        this.checkAndCreateConfig();
-        config = PixelHuntConfig.getInstance(PixelHuntConfig.CONFIG_PATH);
-        PixelHuntFactory.init(PixelHuntConfig.getConfigNode());
 
-        MinecraftForge.EVENT_BUS.register(this.displayTask);
-        MinecraftForge.EVENT_BUS.register(new PokemonSpawnListener(this));
-        Pixelmon.EVENT_BUS.register(new PokemonCaptureListener(this));
-
-        this.commandFactory.registerInjector(PixelHunt.class, (sender, args) -> {
-            if (args.length != 1) {
-                sender.sendMessage(NO_SUCH_HUNT);
-                return null;
-            }
-
-            PixelHunt hunt = PixelHuntFactory.getHunt(args[0]);
-
-            if (hunt == null) {
-                sender.sendMessage(NO_SUCH_HUNT);
-                return null;
-            }
-
-            return hunt;
-        });
-        this.commandFactory.registerCommand(server, new PixelHuntCommand());
-    }
-
-    private void checkAndCreateConfig() {
-        if (!PixelHuntConfig.CONFIG_PATH.toFile().exists()) {
-            try {
-                PixelHuntConfig.CONFIG_PATH.getParent().toFile().mkdir();
-                Files.createFile(PixelHuntConfig.CONFIG_PATH);
-
-                InputStream resourceAsStream = getClass().getClassLoader().getResourceAsStream("config.yml");
-                byte[] buffer = new byte[resourceAsStream.available()];
-                resourceAsStream.read(buffer);
-
-                Files.write(PixelHuntConfig.CONFIG_PATH, buffer);
-            } catch (IOException e) {
-                e.printStackTrace();
-            }
-        }
     }
 
     @Mod.EventHandler
@@ -106,19 +57,7 @@ public class PixelHuntForge {
         return instance;
     }
 
-    public static final MinecraftServer getServer() {
-        return server;
-    }
-
     public PixelHuntConfig getConfig() {
         return this.config;
-    }
-
-    public void setConfig(PixelHuntConfig config) {
-        this.config = config;
-    }
-
-    public ParticleDisplayTask getDisplayTask() {
-        return this.displayTask;
     }
 }
