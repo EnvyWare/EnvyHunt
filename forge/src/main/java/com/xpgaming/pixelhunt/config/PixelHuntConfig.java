dee94ad2ddad534942571e44075cadbbd634feb7
danorris709 <dnorris443@gmail.com>
2021-09-01 15:28:45 +0100

    ref(forge): update config

--- forge/src/main/java/com/xpgaming/pixelhunt/config/PixelHuntConfig.java
+++ forge/src/main/java/com/xpgaming/pixelhunt/config/PixelHuntConfig.java
@@ -1,6 +1,9 @@
 package com.xpgaming.pixelhunt.config;
 
+import com.envyful.api.config.data.ConfigPath;
+import com.envyful.api.config.type.ConfigInterface;
 import com.google.common.collect.Lists;
+import net.minecraft.client.gui.GuiVideoSettings;
 import org.spongepowered.configurate.ConfigurationNode;
 import org.spongepowered.configurate.objectmapping.ConfigSerializable;
 import org.spongepowered.configurate.objectmapping.ObjectMapper;
@@ -12,71 +15,17 @@ import java.nio.file.Path;
 import java.nio.file.Paths;
 import java.util.List;
 
+@ConfigPath("config/PixelHuntRemastered/config.yml")
 @ConfigSerializable
 public class PixelHuntConfig {
 
-    public static final Path CONFIG_PATH = Paths.get("config" + File.separator + "PixelHunt" + File.separator + "config.yml");
+    private ConfigInterface configInterface = new ConfigInterface();
 
-    private static ConfigurationNode configNode;
-
-    public static PixelHuntConfig getInstance(Path path) {
-        try {
-            ObjectMapper<PixelHuntConfig> objectMapper = ObjectMapper.factory().get(PixelHuntConfig.class);
-            YamlConfigurationLoader build = YamlConfigurationLoader.builder().path(path).build();
-
-            configNode = build.load();
-            return objectMapper.load(configNode);
-        } catch (IOException e) {
-            e.printStackTrace();
-        }
-
-        return null;
-    }
-
-    public static ConfigurationNode getConfigNode() {
-        return configNode;
-    }
-
-    private int guiHeight = 3;
-    private String guiName = "PixelHunt";
-    private String backgroundItem = "minecraft:stained_glass_pane";
-    private int backgroundItemDamage = 8;
-    private boolean checkeredBackground = false;
-    private String offColourBackgroundItem = "minecraft:stained_glass_pane";
-    private int offColourBackgroundItemDamage = 9;
     private List<String> spawnBroadcast = Lists.newArrayList();
     private List<String> timeoutBroadcast = Lists.newArrayList();
 
-    public int getGuiHeight() {
-        return this.guiHeight;
-    }
-
-    public String getGuiName() {
-        return this.guiName;
-    }
-
-    public String getBackgroundItem() {
-        return this.backgroundItem;
-    }
-
-    public int getBackgroundItemDamage() {
-        return this.backgroundItemDamage;
-    }
-
     public List<String> getSpawnBroadcast() {
         return this.spawnBroadcast;
-    }
-
-    public boolean isCheckeredBackground() {
-        return this.checkeredBackground;
-    }
-
-    public String getOffColourBackgroundItem() {
-        return this.offColourBackgroundItem;
-    }
-
-    public int getOffColourBackgroundItemDamage() {
-        return this.offColourBackgroundItemDamage;
     }
 
     public List<String> getTimeoutBroadcast() {
