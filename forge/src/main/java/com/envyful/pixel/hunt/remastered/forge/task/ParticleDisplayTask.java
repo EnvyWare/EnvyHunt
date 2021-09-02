38f6af117b4e76a255a7c382c9f50d23782dd8d5
danorris709 <dnorris443@gmail.com>
2021-09-02 08:57:39 +0100

    feat(forge): update ParticleDisplayTask

--- forge/src/main/java/com/envyful/pixel/hunt/remastered/forge/task/ParticleDisplayTask.java
+++ forge/src/main/java/com/envyful/pixel/hunt/remastered/forge/task/ParticleDisplayTask.java
@@ -1,11 +1,12 @@
 package com.envyful.pixel.hunt.remastered.forge.task;
 
-import com.envyful.pixel.hunt.remastered.forge.hunt.PixelHuntForgeFactory;
+import com.envyful.api.concurrency.UtilConcurrency;
+import com.envyful.api.forge.listener.LazyListener;
+import com.envyful.pixel.hunt.remastered.api.PixelHunt;
+import com.envyful.pixel.hunt.remastered.api.PixelHuntFactory;
 import com.google.common.collect.Lists;
 import com.pixelmonmod.pixelmon.api.pokemon.Pokemon;
 import com.pixelmonmod.pixelmon.entities.pixelmon.EntityPixelmon;
-import com.envyful.pixel.hunt.remastered.api.PixelHunt;
-import com.envyful.pixel.hunt.remastered.forge.utils.UtilConcurrency;
 import net.minecraft.util.EnumParticleTypes;
 import net.minecraft.util.math.Vec3d;
 import net.minecraft.world.WorldServer;
@@ -15,14 +16,22 @@ import net.minecraftforge.fml.common.gameevent.TickEvent;
 import java.util.Iterator;
 import java.util.List;
 
-public class ParticleDisplayTask {
+public class ParticleDisplayTask extends LazyListener {
+
+    private static ParticleDisplayTask instance;
 
     private final List<EntityPixelmon> huntPokemon = Lists.newCopyOnWriteArrayList();
 
     private int currentTick = 0;
 
-    public void addPokemon(EntityPixelmon pixelmon) {
-        this.huntPokemon.add(pixelmon);
+    public ParticleDisplayTask() {
+        super();
+
+        instance = this;
+    }
+
+    public static void addPokemon(EntityPixelmon pixelmon) {
+        instance.huntPokemon.add(pixelmon);
     }
 
     @SubscribeEvent
@@ -33,29 +42,30 @@ public class ParticleDisplayTask {
             return;
         }
 
-        UtilConcurrency.runAsync(() -> {
-            Iterator<EntityPixelmon> iterator = this.huntPokemon.iterator();
+        Iterator<EntityPixelmon> iterator = this.huntPokemon.iterator();
 
-            while (iterator.hasNext()) {
-                EntityPixelmon pixelmon = iterator.next();
+        while (iterator.hasNext()) {
 
-                if (pixelmon == null || pixelmon.isDead || pixelmon.hasOwner() || !this.isHuntPokemon(pixelmon.getPokemonData())) {
-                    iterator.remove();
-                    continue;
-                }
+            EntityPixelmon pixelmon = iterator.next();
 
-                WorldServer worldServer = (WorldServer) pixelmon.getEntityWorld();
-                Vec3d positionVector = pixelmon.getPositionVector();
-
-                worldServer.spawnParticle(EnumParticleTypes.FLAME,
-                        positionVector.x,
-                        positionVector.y,
-                        positionVector.z,
-                        5,
-                        0, 0, 0, 0.05);
+            if (pixelmon == null || pixelmon.isDead || pixelmon.hasOwner() || !this.isHuntPokemon(pixelmon.getPokemonData())) {
+                iterator.remove();
+                continue;
             }
 
-            for (PixelHunt allHunt : PixelHuntForgeFactory.getAllHunts()) {
+            WorldServer worldServer = (WorldServer) pixelmon.getEntityWorld();
+            Vec3d positionVector = pixelmon.getPositionVector();
+
+            worldServer.spawnParticle(EnumParticleTypes.FLAME,
+                    positionVector.x,
+                    positionVector.y,
+                    positionVector.z,
+                    5,
+                    0, 0, 0, 0.05);
+        }
+
+        UtilConcurrency.runAsync(() -> {
+            for (PixelHunt allHunt : PixelHuntFactory.getAllHunts()) {
                 if (allHunt.hasTimedOut()) {
                     allHunt.end();
                     allHunt.generatePokemon();
@@ -65,7 +75,7 @@ public class ParticleDisplayTask {
     }
 
     private boolean isHuntPokemon(Pokemon pokemon) {
-        for (PixelHunt hunt : PixelHuntForgeFactory.getAllHunts()) {
+        for (PixelHunt hunt : PixelHuntFactory.getAllHunts()) {
             if (hunt.isBeingHunted(pokemon)) {
                 return true;
             }
