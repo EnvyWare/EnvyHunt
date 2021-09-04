9ba2a4bbd6476581cda770cc61425a5dd1cbe8eb
danorris709 <dnorris443@gmail.com>
2021-09-04 10:37:31 +0100

    fix(forge): relocate reload command

--- forge/src/main/java/com/envyful/pixel/hunt/remastered/forge/commands/PixelHuntCommand.java
+++ forge/src/main/java/com/envyful/pixel/hunt/remastered/forge/commands/PixelHuntCommand.java
@@ -2,15 +2,12 @@ package com.envyful.pixel.hunt.remastered.forge.commands;
 
 
 import com.envyful.api.command.annotate.Command;
-import com.envyful.api.command.annotate.Permissible;
+import com.envyful.api.command.annotate.SubCommands;
 import com.envyful.api.command.annotate.executor.CommandProcessor;
 import com.envyful.api.command.annotate.executor.Sender;
-import com.envyful.pixel.hunt.remastered.api.PixelHuntFactory;
 import com.envyful.pixel.hunt.remastered.forge.PixelHuntForge;
 import com.envyful.pixel.hunt.remastered.forge.ui.HuntUI;
 import net.minecraft.entity.player.EntityPlayerMP;
-import net.minecraft.util.text.ITextComponent;
-import net.minecraft.util.text.TextComponentString;
 
 @Command(
        value = "hunt",
@@ -21,24 +18,12 @@ import net.minecraft.util.text.TextComponentString;
                "pokehunt"
         }
 )
+@SubCommands(ReloadCommand.class)
 public class PixelHuntCommand {
-
-    private static final ITextComponent STARTED_RELOAD = new TextComponentString("Reloading...");
-    private static final ITextComponent RELOADED = new TextComponentString("Reloaded");
-    private static final ITextComponent STARTED = new TextComponentString("Attempting to start hunt");
 
     @CommandProcessor
     public void executeCommand(@Sender EntityPlayerMP sender) {
         HuntUI.open(PixelHuntForge.getInstance().getPlayerManager().getPlayer(sender));
-    }
-
-    @CommandProcessor("reload")
-    @Permissible("pixel.hunt.remastered.command.reload")
-    public void executeReloadCommand(@Sender EntityPlayerMP sender) {
-        sender.sendMessage(STARTED_RELOAD);
-        PixelHuntForge.getInstance().loadConfig();
-        PixelHuntFactory.reloadHunts();
-        sender.sendMessage(RELOADED);
     }
 }
 
---
+++ forge/src/main/java/com/envyful/pixel/hunt/remastered/forge/commands/ReloadCommand.java
@@ -0,0 +1,32 @@
+package com.envyful.pixel.hunt.remastered.forge.commands;
+
+import com.envyful.api.command.annotate.Child;
+import com.envyful.api.command.annotate.Command;
+import com.envyful.api.command.annotate.Permissible;
+import com.envyful.api.command.annotate.executor.CommandProcessor;
+import com.envyful.api.command.annotate.executor.Sender;
+import com.envyful.pixel.hunt.remastered.api.PixelHuntFactory;
+import com.envyful.pixel.hunt.remastered.forge.PixelHuntForge;
+import net.minecraft.entity.player.EntityPlayerMP;
+import net.minecraft.util.text.ITextComponent;
+import net.minecraft.util.text.TextComponentString;
+
+@Command(
+        value = "reoad",
+        description = "Reloads the config"
+)
+@Permissible("pixel.hunt.remastered.command.reload")
+@Child
+public class ReloadCommand {
+
+    private static final ITextComponent STARTED_RELOAD = new TextComponentString("Reloading...");
+    private static final ITextComponent RELOADED = new TextComponentString("Reloaded");
+
+    @CommandProcessor
+    public void executeCommand(@Sender EntityPlayerMP sender) {
+        sender.sendMessage(STARTED_RELOAD);
+        PixelHuntForge.getInstance().loadConfig();
+        PixelHuntFactory.reloadHunts();
+        sender.sendMessage(RELOADED);
+    }
+}
