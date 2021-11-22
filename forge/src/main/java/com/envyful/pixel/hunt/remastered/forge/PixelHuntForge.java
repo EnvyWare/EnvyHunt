3402602c60527e4b39438104988ee9052bf67d5f
Daniel Norris <dnorris443@gmail.com>
2021-11-22 16:30:10 +0000

    chore(gradle): bump version to 2.0.0 inline with Pixelmon sidemods

--- forge/src/main/java/com/envyful/pixel/hunt/remastered/forge/PixelHuntForge.java
+++ forge/src/main/java/com/envyful/pixel/hunt/remastered/forge/PixelHuntForge.java
@@ -29,7 +29,7 @@ import java.io.IOException;
 )
 public class PixelHuntForge {
 
-    public static final String MOD_VERSION = "1.0.1";
+    public static final String MOD_VERSION = "2.0.0";
 
     private static PixelHuntForge instance;
 
