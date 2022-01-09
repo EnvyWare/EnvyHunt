1c862d450be2e5c22172bfb740b409f15a3da94e
Daniel Norris <dnorris443@gmail.com>
2022-01-09 15:19:44 +0000

    chore(gradle): bump version to 2.1.3

--- forge/src/main/java/com/envyful/pixel/hunt/remastered/forge/PixelHuntForge.java
+++ forge/src/main/java/com/envyful/pixel/hunt/remastered/forge/PixelHuntForge.java
@@ -29,7 +29,7 @@ import java.io.IOException;
 )
 public class PixelHuntForge {
 
-    public static final String MOD_VERSION = "2.1.2";
+    public static final String MOD_VERSION = "2.1.3";
 
     private static PixelHuntForge instance;
 
