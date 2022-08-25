0ba5cc32530bdcdf841677df6a123cc03356913b
Daniel Norris <dnorris443@gmail.com>
2022-08-25 17:30:23 +0100

    fix(forge16): prevent weird UI crash

--- forge/src/main/java/com/envyful/pixel/hunt/remastered/forge/PixelHuntForge.java
+++ forge/src/main/java/com/envyful/pixel/hunt/remastered/forge/PixelHuntForge.java
@@ -29,7 +29,7 @@ import java.io.IOException;
 )
 public class PixelHuntForge {
 
-    public static final String MOD_VERSION = "3.0.2";
+    public static final String MOD_VERSION = "3.2.0";
 
     private static PixelHuntForge instance;
 
