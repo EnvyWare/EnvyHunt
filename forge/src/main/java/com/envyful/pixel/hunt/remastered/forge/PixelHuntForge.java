ce118011041a1f5402dc9e7425a4ba3a68e99651
Daniel Norris <dnorris443@gmail.com>
2023-05-24 11:49:55 +0100

    chore(gradle): bump version to 4.0.3

--- forge/src/main/java/com/envyful/pixel/hunt/remastered/forge/PixelHuntForge.java
+++ forge/src/main/java/com/envyful/pixel/hunt/remastered/forge/PixelHuntForge.java
@@ -29,7 +29,7 @@ import java.io.IOException;
 )
 public class PixelHuntForge {
 
-    public static final String MOD_VERSION = "4.0.2";
+    public static final String MOD_VERSION = "4.0.3";
 
     private static PixelHuntForge instance;
 
